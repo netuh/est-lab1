3f17cc71f65fede75c3ddbbf6bce92620e003cb2
Ezequiel Henrique <ezequielhenrique806@gmail.com>
2022-12-06 20:33:36 +0000

    Metodos insert, delete and search

--- ListaArray.java
+++ ListaArray.java
@@ -1,20 +1,56 @@
+import java.util.Arrays;
+
 public class ListaArray implements EstruturaDeDados{
+    private int[] array;
+    private int contador;
+
+    public ListaArray() {
+        array = new int[1000];
+        contador = 0;
+    }
 
     @Override
     public boolean insert(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+        if (contador < this.array.length) {
+            this.array[contador] = chave;
+            contador += 1;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+        boolean findNumber = false;
+
+        for (int i=0; i<this.array.length; i++) {
+            if (!findNumber) {
+                if (this.array[i] == chave) {
+                    findNumber = true;
+
+                    this.array[i] = 0;
+                    contador -= 1;
+                }
+            } else {
+                int aux = this.array[i];
+                this.array[i-1] = aux;
+            }
+        }
+        if (findNumber) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO Auto-generated method stub
+        for (int i=0; i<this.array.length; i++) {
+            if (this.array[i] == chave) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -42,7 +78,24 @@ public class ListaArray implements EstruturaDeDados{
         return 0;
     }
 
+    public String getArray() {
+        return Arrays.toString(this.array);
+    }
+
     public static void main(String[] args) {
         ListaArray r = new ListaArray();
+        r.insert(5);
+        r.insert(10);
+        r.insert(15);
+        r.insert(20);
+        r.insert(30);
+        r.insert(50);
+        System.out.println(r.getArray());
+
+        System.out.println(r.search(15));
+        r.delete(5);
+        System.out.println(r.getArray());
+        r.delete(20);
+        System.out.println(r.getArray());
     }
 }
